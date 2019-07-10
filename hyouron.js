9721b572aef0fcb0a66834d20e68c0933451de5a
aidatorajiro <kawarusosu@zoho.com>
2019-07-10 23:11:03 +0900

    Implement hyouron.js

--- hyouron.js
+++ hyouron.js
@@ -1,2 +1,41 @@
 "情報同士の関わり合いには一定のパターンがある。例えば、「Aという情報はBという媒体によって強化される。そのことはCという事実に体現される。」といったように。この文章内ではA B Cはそれぞれ型のようなものを持っている。Cが具体的 Bの形容詞的 Aが何かしら強度をもつ情報であること。などである。評論は「事実=>>考察」「事実=>>考察」「事実=>>考察」「事実=>>考察」「事実=>>考察」「事実=>>考察」「事実=>>考察」「事実=>>考察」「事実=>>考察」 ... というのが普通だけど、それは本質ではない。評論というものが表現する、情報同士の関係性のダイアグラム、およびそこから示唆される世界全体の情報の流れというものが本質なのである。これは言い換えると、個々の具体的な言説から離れ、抽象的な構造を考える、SF的にみるということである。"
 
+let registerAtom
+let registerConjunction
+let validateSentence
+
+{
+    let Combinators = {}
+
+    /**
+     * Register an atom.
+     */
+    registerAtom = function (name) {
+        Combinators[name] = JSON.stringify({type: "atom"})
+    }
+
+    /**
+     * Register a conjunction.
+     */
+    registerConjunction = function (name, num_args) {
+        Combinators[name] = JSON.stringify({type: "conj", num_args})
+    }
+
+    /**
+     * Validate a sentence.
+     * @param {Array} sentence
+     */
+    validateSentence = function (sentence) {
+        let name = sentence[0]
+        let params = JSON.parse(Combinators[name])
+        if (params.type === "atom" && sentence.length === 1) {
+            return true
+        }
+        if (params.type === "conj" && sentence.length === params.num_args + 1) {
+            return sentence.slice(1).reduce((a, b) => (a && validateSentence(b)), true)
+        }
+        return false
+    }
+}
+
+export {registerAtom, registerConjunction, validateSentence}
--- recursive_ctx.js
+++ recursive_ctx.js
@@ -1,34 +1,6 @@
 "情報を得る。情報を使う。それによって情報を得る。情報を使う。それによって情報を得る。情報を使う。それによって情報を得る。情報を使う。それによって情報を得る。情報を使う。それによって情報を得る。情報を使う。それによって情報を得る。情報を使う。それによって情報を得る。情報を使う。それによって情報を得る。情報を使う。それによって情報を得る。情報を使う。それによって情報を得る。情報を使う。それによって情報を得る。情報を使う。それによって情報を得る。情報を使う。それによって情報を得る。情報を使う。それによって情報を得る。情報を使う。それによって情報を得る。情報を使う。それによって情報を得る。情報を使う。それによって情報を得る。情報を使う。それによって情報を得る。情報を使う。それによって情報を得る。情報を使う。それによって情報を得る。情報を使う。それによって情報を得る。情報を使う。それによって情報を得る。情報を使う。それによって情報を得る。情報を使う。それによって情報を得る。情報を使う。それによって情報を得る。情報を使う。それによって情報を得る。情報を使う。それによって情報を得る。情報を使う。それによって情報を得る。情報を使う。それによって情報を得る。情報を使う。それによって情報を得る。情報を使う。それによって情報を得る。情報を使う。それによって情報を得る。........"
 
-// Exposure to updateFuncs / getFieldValue
-let pushInfo;
-let getInfo;
-let getFieldSize;
-
-{
-  let field = []
-
-  /**
-   * Push an information to the information field.
-   */
-  pushInfo = function (info) {
-    field.push(info)
-  }
-
-  /**
-   * Get an information from the information field.
-   */
-  getInfo = function (i) {
-    return field[i]
-  }
-
-  /**
-   * Get the length of the field.
-   */
-  getFieldSize = function (i) {
-    return field.length
-  }
-}
+import {pushInfo, getInfo, getFieldSize} from "./info_field.js"
 
 // Exposure to outer world
 let getFieldValue;
