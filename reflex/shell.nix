e84747544e01eabfae23816b1e445735b6676f6f
aidatorajiro <kawarusosu@zoho.com>
2023-01-22 15:24:18 +0900

    add shell initialization script

--- reflex/shell.nix
+++ reflex/shell.nix
@@ -1,6 +1,6 @@
 { pkgs ? import <nixpkgs> {} }:
 pkgs.haskellPackages.shellFor {
-    packages = _: [ (pkgs.haskellPackages.callCabal2nix "MyReflexProject" ./. {}) ];
+    packages = _: [ (pkgs.haskellPackages.callCabal2nix "MyReflexProject" ./frontend {}) ];
     buildInputs = [
       pkgs.cabal-install
       pkgs.haskell-language-server
