a8fd7963e8fb2175c12910c270024b767dcf7d03
aidatorajiro <kawarusosu@zoho.com>
2023-01-22 22:08:05 +0900

    add more ghc build files

--- reflex/build-src.sh
+++ reflex/build-src.sh
@@ -1,3 +1,4 @@
 docker exec reflex /bin/bash -c "sudo rm -rf ~/Prophyan"
 docker cp ../ reflex:/home/nix/Prophyan
-docker exec reflex /bin/bash -c "sudo chown -R nix:nix ~/Prophyan; sudo chmod -R 755 ~/Prophyan; cd ~/Prophyan/reflex; . ~/e.sh; nix-build -o ./build-ghcjs -A ghcjs.MyReflexProject --show-trace"
+docker exec reflex /bin/bash -c "sudo chown -R nix:nix ~/Prophyan; sudo chmod -R 755 ~/Prophyan"
+docker exec reflex /bin/bash -c "cd ~/Prophyan/reflex; . ~/e.sh; nix-build -o ./build-ghcjs -A ghcjs.MyReflexProject --show-trace"
--- reflex/ssh-and-env.sh
+++ reflex/ssh-and-env.sh
@@ -5,6 +5,8 @@ docker exec reflex /bin/bash -c "mkdir /home/nix/.ssh/"
 docker cp ~/.ssh/id_ed25519.pub reflex:/home/nix/.ssh/authorized_keys
 docker exec reflex /bin/bash -c "sudo chown nix:nix /home/nix/.ssh/authorized_keys"
 docker exec reflex /bin/bash -c "sudo chmod 600 /home/nix/.ssh/authorized_keys"
+docker exec reflex /bin/bash -c "echo X11Forwarding yes | sudo tee -a /etc/ssh/sshd_config"
+docker exec reflex /bin/bash -c "echo X11UseLocalhost no | sudo tee -a /etc/ssh/sshd_config"
 
 docker exec reflex /bin/bash -c "cd ~/Prophyan/reflex; . ~/e.sh; nix-shell --run \"env > ~/envfile\""
 docker exec reflex /bin/bash -c "cp ~/Prophyan/reflex/bashrc ~/.bashrc"
