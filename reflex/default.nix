ed75417b480a82a4c2f63c248b4273f6c2b3a1b3
aidatorajiro <kawarusosu@zoho.com>
2023-01-22 15:48:48 +0900

    add some comment

--- reflex/default.nix
+++ reflex/default.nix
@@ -2,7 +2,7 @@
     system ? builtins.currentSystem,
     reflex-platform ? fetchGit {
         url = "https://github.com/reflex-frp/reflex-platform.git";
-        rev = "12ab0adac1f3f1869e256e2f7fcd9f4ff1394d92";
+        rev = "12ab0adac1f3f1869e256e2f7fcd9f4ff1394d92"; /* As of Jan 22, 2023 */
     }
 }:
 (import reflex-platform {
