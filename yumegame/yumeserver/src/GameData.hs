5636df74334fbb4182d2792a0748824cb9f7469a
aidatorajiro <kawarusosu@zoho.com>
2023-03-20 04:37:01 +0900

    implement websocket server

--- yumegame/yumeserver/src/GameData.hs
+++ yumegame/yumeserver/src/GameData.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE Arrows #-}
 {-# LANGUAGE TemplateHaskell #-}
 
-module GameData (Vec3, Object, GameInput, GameOutput, key, newObj, soundFeed, playerPos, newGameOutput) where
+module GameData (Vec3, Object, GameInput, GameOutput, isEmptyGameOutput, newGameOutput, newGameInput) where
 
 import FRP.Yampa ( integral, time, returnA, SF )
 
@@ -14,27 +14,27 @@ import Data.Aeson.TH
 
 import Control.Lens ( makeLenses )
 
-newtype Vec3 = Vec3 (Double, Double, Double)
+newtype Vec3 = Vec3 (Double, Double, Double)  deriving( Eq )
 $(deriveJSON defaultOptions ''Vec3)
 
 data Object =
   Cube String Vec3 Vec3 Vec3 |
-  Sphere String Vec3 Vec3 Vec3
+  Sphere String Vec3 Vec3 Vec3  deriving( Eq )
 $(deriveJSON defaultOptions ''Object)
 
-data UIElement = Button String Vec3
+data UIElement = Button String Vec3  deriving( Eq )
 $(deriveJSON defaultOptions ''UIElement)
 
-newtype UIConfig = UIConfig [UIElement]
+newtype UIConfig = UIConfig [UIElement]   deriving( Eq )
 $(deriveJSON defaultOptions ''UIConfig)
 
-data GameInput = GameInput { _key :: Maybe Int, _uiClick :: Maybe String }
+data GameInput = GameInput { _key :: Maybe Int, _uiClick :: Maybe String } deriving( Eq )
 makeLenses ''GameInput
 $(deriveJSON defaultOptions{fieldLabelModifier = drop 1} ''GameInput)
 newGameInput = GameInput { _key = Nothing, _uiClick = Nothing }
 
-data GameOutput = GameOutput { _newObj :: Maybe Object, _soundFeed :: Maybe [Int], _playerPos :: Maybe Vec3, _updateUI :: Maybe UIConfig }
+data GameOutput = GameOutput { _newObj :: Maybe Object, _soundFeed :: Maybe [Int], _playerPos :: Maybe Vec3, _updateUI :: Maybe UIConfig }  deriving( Eq )
 makeLenses ''GameOutput
 $(deriveJSON defaultOptions{fieldLabelModifier = drop 1} ''GameOutput)
 newGameOutput = GameOutput { _newObj = Nothing, _soundFeed = Nothing, _playerPos = Nothing, _updateUI = Nothing }
-
+isEmptyGameOutput x = x == newGameOutput
--- yumegame/yumeserver/src/LibMain.hs
+++ yumegame/yumeserver/src/LibMain.hs
@@ -1,11 +1,12 @@
 {-# LANGUAGE Arrows #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE BangPatterns #-}
 
 module LibMain ( startApp ) where
 
-import FRP.Yampa ( integral, time, returnA, SF )
+import FRP.Yampa ( integral, time, returnA, SF, reactimate )
 
-import Control.Lens ( makeLenses )
+import Control.Lens ( makeLenses, (&), (.~), (?~) )
 
 import Data.Aeson.TH
     ( deriveJSON,
@@ -16,43 +17,62 @@ import Data.Char (toLower)
 
 import Data.Aeson.Casing ( snakeCase )
 
-import GameData ( GameInput, GameOutput, newGameOutput )
+import GameData ( GameInput, GameOutput, newGameOutput, newGameInput, isEmptyGameOutput )
 
-import Network.Wai
-    ( Request(requestHeaders),
-      Application,
-      responseBuilder,
-      getRequestBodyChunk )
-import Network.Wai.Handler.Warp ( run )
-import Network.HTTP.Types ( status200, status400 )
 import Data.Foldable (find)
 import qualified Data.ByteString as B
 import Data.Aeson (decode, encode)
 import qualified Data.ByteString.Builder as BB
+import Network.WebSockets (runServer, ServerApp, acceptRequest, receive, Message (ControlMessage, DataMessage), receiveDataMessage, sendTextData)
+import qualified Network.WebSockets as W
+import Control.Concurrent (forkIO, newEmptyMVar)
+import Control.Concurrent.STM (newTQueue, newTQueueIO, atomically, writeTQueue, readTQueue, flushTQueue, tryReadTQueue, newTVar, newTVarIO, writeTVar, readTVar)
+import Control.Monad
 
 yumeserver :: SF GameInput GameOutput
 yumeserver = proc x -> do
   let def = newGameOutput
   returnA -< def
 
-webApp :: Application
-webApp req respond = do
-  let headers = requestHeaders req
-      baseHeader = [("Content-Type", "application/json"), ("Access-Control-Allow-Origin", "*")]
-
-  case find (\(x, y) -> x == "Content-Type") headers of
-      Just (x, y) -> do
-            let getAllBody = do
-                  chunk <- getRequestBodyChunk req
-                  if chunk == "" then return [] else do
-                      body <- getAllBody
-                      return (chunk : body)
-            body <- B.concat <$> getAllBody
-            let parsed = decode (B.fromStrict body) :: Maybe GameInput
-            case parsed of
-                Just x -> respond $ responseBuilder status200 baseHeader (BB.byteString body)
-                Nothing -> respond $ responseBuilder status400 baseHeader "{\"error\": \"ERROR! json parse failed\"}"
-      Nothing -> respond $ responseBuilder status400 baseHeader "{\"error\": \"ERROR! please set Content-Type to application/json\"}"
-
 startApp :: IO ()
-startApp = run 9435 webApp
+startApp = do
+  cnt <- newTVarIO (0 :: Int)
+  input_queue <- newTQueueIO
+  output_queue <- newTQueueIO
+  forkIO $ reactimate (return newGameInput)
+    (\b -> do
+      x <- atomically (tryReadTQueue input_queue)
+      case x of
+        Just _ -> return (0.1, x)
+        Nothing -> return (0.1, Nothing))
+    (\b o -> do
+      unless (isEmptyGameOutput o) $ atomically (writeTQueue output_queue o)
+
+      t <- atomically $ do
+        t <- readTVar cnt
+        writeTVar cnt (t + 1)
+        return t
+      print t
+
+      return False) yumeserver
+  
+  runServer "0.0.0.0" 9435 (\req -> do
+      con <- acceptRequest req
+      let procA = do
+            mes <- receiveDataMessage con
+            case mes of
+              W.Text bs _ -> do
+                case decode bs :: Maybe GameInput of
+                  Just i -> atomically (writeTQueue input_queue i)
+                  Nothing -> return ()
+            procA
+
+          procB = do
+            o <- atomically (readTQueue output_queue)
+            sendTextData con (encode (o :: GameOutput))
+            procB
+
+      forkIO procA
+      procB
+
+      return ())
